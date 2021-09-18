bf537931f247bfa0ca3550a147b28eb475d3ef0b
farhanfuadabir <farhanfuad.abir@gmail.com>
2021-09-18 08:30:53 +0600

    Add video duration

--- main.py
+++ main.py
@@ -1,59 +1,70 @@
 import os
 import cv2
+import time
 
-filename = 'video.avi'
-frames_per_second = 24.0
+name = 'trial'
+file_format = '.mp4'
+fps = 24.0
 res = '480p'
+duration = 30
 
-# Set resolution for the video capture
-# Function adapted from https://kirr.co/0l6qmh
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
-
-# Standard Video Dimensions Sizes
-STD_DIMENSIONS =  {
+# Standard Video Dimensions
+STD_DIMENSIONS = {
     "480p": (640, 480),
     "720p": (1280, 720),
     "1080p": (1920, 1080),
     "4k": (3840, 2160),
 }
 
+
+# Set resolution for the video capture
+def change_res(cap, width, height):
+    cap.set(3, width)
+    cap.set(4, height)
+
+
 # grab resolution dimensions and set video capture to it.
 def get_dims(cap, res='1080p'):
     width, height = STD_DIMENSIONS["480p"]
     if res in STD_DIMENSIONS:
-        width,height = STD_DIMENSIONS[res]
-    ## change the current caputre device
-    ## to the resulting resolution
+        width, height = STD_DIMENSIONS[res]
+    # change the current capture device to the resulting resolution
     change_res(cap, width, height)
     return width, height
+
 
 # Video Encoding, might require additional installs
 # Types of Codes: http://www.fourcc.org/codecs.php
 VIDEO_TYPE = {
     'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
+    'mp4': cv2.VideoWriter_fourcc(*'mp4v'),
+    # 'mp4': cv2.VideoWriter_fourcc(*'XVID'),
 }
+
 
 def get_video_type(filename):
     filename, ext = os.path.splitext(filename)
     if ext in VIDEO_TYPE:
-      return  VIDEO_TYPE[ext]
-    return VIDEO_TYPE['avi']
+        return VIDEO_TYPE[ext]
+    return VIDEO_TYPE['mp4']
 
 
-cap = cv2.VideoCapture(0)
-out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cap, res))
+if __name__ == '__main__':
+    name = input("Enter the name of the gesture: ")
+    filename = name + file_format
 
-while True:
-    ret, frame = cap.read()
-    out.write(frame)
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    cap = cv2.VideoCapture(0)
+    out = cv2.VideoWriter(filename, get_video_type(filename), fps, get_dims(cap, res))
 
-cap.release()
-out.release()
-cv2.destroyAllWindows()
+    init_time = time.time()
+
+    while (time.time() - init_time) < duration:
+        ret, frame = cap.read()
+        out.write(frame)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    out.release()
+    cv2.destroyAllWindows()
